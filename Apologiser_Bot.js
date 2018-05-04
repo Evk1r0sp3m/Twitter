e1ac5338a9e6ca65704ff70c4878393399d72b21
Evk1r0Sp3m <38351962+Evk1r0sp3m@users.noreply.github.com>
2018-05-04 12:47:00 +0100

    Update Apologiser_Bot.js
    Fixed references to Twitter_API_Search.js

--- Apologiser_Bot.js
+++ Apologiser_Bot.js
@@ -10,7 +10,7 @@ var T = new Twit(config)
 
 
 var userId = 'INSERT_USERID';
-//Get "userID" of twitter user from running hello.js with "screen_name"
+//Get "userID" of twitter user from running Twitter_API_Search.js with "screen_name"
 var stream = T.stream('statuses/filter', { follow: userId });
 stream.on('tweet', GotTweet);
 
@@ -36,53 +36,3 @@ function GotTweet(tweet) {
     	}
     })
 };
-
-
-
-
-//The following can be used to pull data about specific user's tweets:
-
-// var userId = 'INSERT_USERID';
-// //Get "userID" of twitter user from running hello.js with "screen_name"
-// var stream = T.stream('statuses/filter', { follow: userId });
-// stream.on('tweet', GotTweet);
-
-// function GotTweet(tweet) {
-// 	console.log(tweet);
-// }
-
-
-
-
-//The following can be used to automate reply's to mentions (Spam)
-
-// var stream = T.stream('statuses/filter', { track: ['@myTwitterHandle'] });
-// stream.on('tweet', tweetEvent);
-
-// function tweetEvent(tweet) {
-
-//     // Who sent the tweet?
-//     var name = tweet.user.screen_name;
-//     // What is the text?
-//     // var txt = tweet.text;
-//     // the status update or tweet ID in which we will reply
-//     var nameID  = tweet.id_str;
-
-//      // Get rid of the @ mention
-//     // var txt = txt.replace(/@myTwitterHandle/g, "");
-
-//     // Start a reply back to the sender
-//     var reply = "You mentioned me! @" + name + ' ' + 'You are super cool!';
-//     var params             = {
-//                               status: reply,
-//                               in_reply_to_status_id: nameID
-//                              };
-
-//     T.post('statuses/update', params, function(err, data, response) {
-//       if (err !== undefined) {
-//         console.log(err);
-//       } else {
-//         console.log('Tweeted: ' + params.status);
-//       }
-//     })
-// };
